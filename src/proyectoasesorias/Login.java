fd862205ecd8163ea8eea80df430f263a0c85d2f
fabianzacate <fabianzacate@192.168.9.84>
2018-04-11 01:21:52 -0500

    cambie el color del texto de la aplicacion

--- src/proyectoasesorias/Login.java
+++ src/proyectoasesorias/Login.java
@@ -24,6 +24,7 @@ import javafx.event.*;
 import javafx.geometry.Pos;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Reflection;
 import javafx.scene.image.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -95,7 +96,13 @@ public class Login extends Application {
         ivPass.setSmooth(true);
 
         lblIniciar = new Label(" ADVIUAM ");//Aqui puede ir el logo de la aplicacion
-        lblIniciar.setFont(Font.font("CASTELLAR", FontWeight.EXTRA_LIGHT, FontPosture.ITALIC, 25));
+        lblIniciar.setFont(Font.font("CASTELLAR", FontWeight.EXTRA_LIGHT, FontPosture.ITALIC, 40));
+        lblIniciar.setTextFill(Color.web("#E91E63"));
+        lblIniciar.setStyle("-fx-background-color: #FFC107");
+        
+        Reflection r = new Reflection();
+        r.setFraction(0.9);
+        lblIniciar.setEffect(r);
         
         //lblIniciar.setFont(Font.font("SEGOE PRINT", FontWeight.MEDIUM, FontPosture.ITALIC, 22));
         //lblIniciar.setFont(Font.font("FORTE", FontWeight.MEDIUM, FontPosture.ITALIC, 22));
